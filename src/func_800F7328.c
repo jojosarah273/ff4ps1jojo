cb6631befafcc8bf3a37333b5751f68f2c4bb906
jojosarah273 <jojosarah273@users.noreply.github.com>
2026-09-08 02:47:14 +0000

    grind hand batch: mapper-read/write + flag variants (6); C-written 1209

--- src/func_800F7328.c
+++ src/func_800F7328.c
@@ -1,12 +1,7 @@
 #include "common.h"
-__asm__(
-  ".globl func_800F7328\n"
-  ".type func_800F7328, @function\n"
-  "func_800F7328:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\tlw $v0, %gp_rel(D_8019ED60)($gp)\n\taddiu $sp, $sp, -0x18\n\tsw $ra, 0x10($sp)\n\tlbu $v1, 0x0($v0)\n\tjal func_800F3B04\n\taddu $a0, $a0, $v1\n\tlw $a0, %gp_rel(D_8019ED5C)($gp)\n\tlbu $v1, 0x0($v0)\n\tlw $ra, 0x10($sp)\n\tsb $v1, 0x0($a0)\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_800F7328, .-func_800F7328\n"
-);
+extern u8 *D_8019ED60;
+extern u8 *D_8019ED5C;
+void func_800F7328(u32 a0)
+{
+    *D_8019ED5C = *(u8 *)func_800F3B04(a0 + *D_8019ED60);
+}
