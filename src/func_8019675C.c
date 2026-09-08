245cf9c4bfe71c820c13c3e03b5e1a0446daacc9
jojosarah273 <jojosarah273@users.noreply.github.com>
2026-09-08 21:30:54 +0000

    fix: 8019675C labels; 2129

--- src/func_8019675C.c
+++ src/func_8019675C.c
@@ -5,14 +5,14 @@ void func_8019675C(void)
        801963C0/801968CC rows; 80196888 tail. */
     func_80196898();
     goto L196794;
-L19677c:
+L19677C:
     func_801963C0();
     func_801968CC();
     if (func_800F53D4() != 0)
         goto L196888;
 L196794:
     /* v1/v0 gate -> L1967C8 / L19677C */
-    goto L19677C;
+    goto L19677C; /* v1/v0 gate (fall: 801963C0 row) */
     /* L1967B8 loop */
     func_801968CC();
     if (func_800F53D4() != 0)
