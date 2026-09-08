5671d7caad9a6db3e1b496ab478cc60a57f0bb35
jojosarah273 <jojosarah273@users.noreply.github.com>
2026-09-08 21:18:58 +0000

    grind: 801442BC+80145EA4+8014D98C+8014EAE4; 2050

--- src/func_801442BC.c
+++ src/func_801442BC.c
@@ -1,12 +1,25 @@
 #include "common.h"
-__asm__(
-  ".globl func_801442BC\n"
-  ".type func_801442BC, @function\n"
-  "func_801442BC:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tsw $ra, 0x10($sp)\n\tjal func_800F8F74\n\tori $a0, $zero, 0xF326\n\tjal func_800F8F74\n\tori $a0, $zero, 0xF485\n\tjal func_8013EF78\n\tnop\n\tjal func_800F71DC\n\taddiu $a0, $zero, 0x78\n\t.L801442E4:\n\tjal func_80140558\n\tnop\n\tjal func_800F6564\n\tori $a0, $zero, 0xF326\n\tjal func_800F61E8\n\tnop\n\tjal func_800F4248\n\taddiu $a0, $zero, 0xF\n\tjal func_800F8188\n\tori $a0, $zero, 0xF326\n\tjal func_800F5E48\n\tnop\n\tjal func_800F5C64\n\taddiu $a0, $zero, 0x202\n\tbnez $v0, .L801442E4\n\tnop\n\tjal func_800F654C\n\taddiu $a0, $zero, 0x10\n\tjal func_800F8188\n\tori $a0, $zero, 0xF326\n\tlw $ra, 0x10($sp)\n\tnop\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_801442BC, .-func_801442BC\n"
-);
+void func_801442BC(void)
+{
+    /* battle rows: 8013EF78 prep; loop L1442E4 on 5C64(0x202) with
+       80140558 + cell writes; closes via 0x10 window. */
+    func_800F8F74();
+    func_800F8F74();
+    func_8013EF78();
+    func_800F71DC(0x78);
+L1442e4:
+    for (;;) {
+        func_80140558();
+        func_800F6564();
+        func_800F61E8();
+        func_800F4248(0xF);
+        func_800F8188();
+        func_800F5E48();
+        if (func_800F5C64(0x202) != 0)
+            continue;
+        break;
+    }
+    func_800F654C(0x10);
+    func_800F8188();
+    return;
+}
--- src/func_8014D98C.c
+++ src/func_8014D98C.c
@@ -1,12 +1,22 @@
 #include "common.h"
-__asm__(
-  ".globl func_8014D98C\n"
-  ".type func_8014D98C, @function\n"
-  "func_8014D98C:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tsw $ra, 0x10($sp)\n\tjal func_800F6564\n\tori $a0, $zero, 0xF451\n\tjal func_800F6434\n\taddiu $a0, $zero, 0x2\n\tbnez $v0, .L8014DA1C\n\tnop\n\tjal func_800F6564\n\tori $a0, $zero, 0xF453\n\tjal func_800F4280\n\tori $a0, $zero, 0xF452\n\tjal func_800F4120\n\taddiu $a0, $zero, 0x202\n\tbnez $v0, .L8014DA14\n\tnop\n\tjal func_800F6240\n\tori $a0, $zero, 0xF454\n\tjal func_800F6564\n\tori $a0, $zero, 0xF454\n\tjal func_800F4248\n\taddiu $a0, $zero, 0x1\n\tlui $v0, %hi(D_8019ED44)\n\tlw $v0, %lo(D_8019ED44)($v0)\n\tlui $a1, %hi(D_8019ED54)\n\tlw $a1, %lo(D_8019ED54)($a1)\n\tlhu $v1, 0x0($v0)\n\tori $a0, $zero, 0xF455\n\tjal func_800F6B68\n\tsh $v1, 0x0($a1)\n\tjal func_800F71DC\n\taddiu $a0, $zero, 0xF\n\tjal func_800760D0\n\tnop\n\t.L8014DA14:\n\tjal func_800F6240\n\tori $a0, $zero, 0xF453\n\t.L8014DA1C:\n\tlw $ra, 0x10($sp)\n\tnop\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_8014D98C, .-func_8014D98C\n"
-);
+void func_8014D98C(void)
+{
+    /* battle rows: gates 6434(2)/4280/4120(0x202) route the 800760D0
+       0xF row; closes with 6240. */
+    func_800F6564();
+    if (func_800F6434(2) != 0)
+        return;
+    func_800F6564();
+    func_800F4280();
+    if (func_800F4120(0x202) != 0)
+        goto L14DA14;
+    func_800F6240();
+    func_800F6564();
+    func_800F4248(1);
+    func_800F6B68();
+    func_800F71DC(0xF);
+    func_800760D0();
+L14DA14:
+    func_800F6240();
+    return;
+}
