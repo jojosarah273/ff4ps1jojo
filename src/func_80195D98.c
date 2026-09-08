0be2b78c9e1d89b01de193f6d17c8be40d64f352
jojosarah273 <jojosarah273@users.noreply.github.com>
2026-09-08 21:43:20 +0000

    fix: 80195D98 spin loop; 2212

--- src/func_80195D98.c
+++ src/func_80195D98.c
@@ -13,18 +13,12 @@ L195DF8:
 L195E34:
     goto L195FF8;
 L195E38:
+    /* v0 gate at L195E6C: 801968CC spin rows until v0 != 0 */
+    do {
+        if (func_801968CC() != 0)
+            goto L195FF8;
+    } while (func_800F53D4() != 0);
     goto L195FF8;
-L195E6C:
-    for (;;) {
-        func_801968CC();
-        if (func_800F53D4() != 0)
-            break;
-        if (func_800F53D4() != 0)
-            continue;
-        goto L195E9C;
-    }
-    /* s0/v0 + s0/v1 latch loops L195F88/L195FA8 */
-    return;
 L195FF8:
     return;
 }
