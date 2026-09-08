8db9d79476fe38fb72acba8d969d3c4fff85a108
jojosarah273 <jojosarah273@users.noreply.github.com>
2026-09-08 02:56:02 +0000

    grind: poll-loop, swap-copy, reset, fill+call (4); C-written 1288

--- src/func_80187C54.c
+++ src/func_80187C54.c
@@ -1,12 +1,16 @@
 #include "common.h"
-__asm__(
-  ".globl func_80187C54\n"
-  ".type func_80187C54, @function\n"
-  "func_80187C54:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tsw $ra, 0x10($sp)\n\t.L80187C5C:\n\tjal func_801928E8\n\taddu $a0, $zero, $zero\n\taddu $a0, $zero, $zero\n\tjal func_8018746C\n\taddu $a1, $zero, $zero\n\tlh $v0, %gp_rel(D_8019EEB8)($gp)\n\tnop\n\tbgtz $v0, .L80187C5C\n\taddiu $v0, $zero, 0x1\n\tlhu $v1, %gp_rel(D_8019EF0C)($gp)\n\tlw $ra, 0x10($sp)\n\tsw $zero, %gp_rel(D_8019EF28)($gp)\n\tsw $v0, %gp_rel(D_8019EF24)($gp)\n\tsh $v1, %gp_rel(D_8019EEF6)($gp)\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_80187C54, .-func_80187C54\n"
-);
+extern s16 *D_8019EEB8;
+extern u16 *D_8019EF0C;
+extern u32 *D_8019EF28;
+extern u32 *D_8019EF24;
+extern u16 *D_8019EEF6;
+void func_80187C54(void)
+{
+    do {
+        func_801928E8(0);
+        func_8018746C(0, 0);
+    } while (*D_8019EEB8 > 0);
+    *D_8019EF28 = 0;
+    *D_8019EF24 = 1;
+    *D_8019EEF6 = *D_8019EF0C;
+}
--- src/func_80187CD0.c
+++ src/func_80187CD0.c
@@ -1,12 +1,20 @@
 #include "common.h"
-__asm__(
-  ".globl func_80187CD0\n"
-  ".type func_80187CD0, @function\n"
-  "func_80187CD0:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\tsh $a0, %gp_rel(D_8019EDAA)($gp)\n\tbnez $a0, .L80187CEC\n\tlui $v0, %hi(D_801CF568)\n\tlui $v1, (0x800D2200 >> 16)\n\tori $v1, $v1, (0x800D2200 & 0xFFFF)\n\tj .L80187CF8\n\taddiu $a0, $v0, %lo(D_801CF568)\n\t.L80187CEC:\n\taddiu $v1, $v0, %lo(D_801CF568)\n\tlui $a0, (0x800D2200 >> 16)\n\tori $a0, $a0, (0x800D2200 & 0xFFFF)\n\t.L80187CF8:\n\taddiu $a1, $zero, 0x7FF\n\t.L80187CFC:\n\tlbu $v0, 0x0($v1)\n\taddiu $v1, $v1, 0x1\n\taddiu $a1, $a1, -0x1\n\tsb $v0, 0x0($a0)\n\tbgez $a1, .L80187CFC\n\taddiu $a0, $a0, 0x1\n\tjr $ra\n\tnop\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_80187CD0, .-func_80187CD0\n"
-);
+extern u16 *D_8019EDAA;
+extern u8 D_801CF568[];
+void func_80187CD0(u16 a0)
+{
+    u8 *dst;
+    u8 *src;
+    *D_8019EDAA = a0;
+    if (a0 != 0) {
+        dst = (u8 *)0x800D2200u;
+        src = (u8 *)D_801CF568;
+    } else {
+        dst = (u8 *)D_801CF568;
+        src = (u8 *)0x800D2200u;
+    }
+    s32 n = 0x7FF;
+    do {
+        *dst++ = *src++;
+    } while (--n >= 0);
+}
