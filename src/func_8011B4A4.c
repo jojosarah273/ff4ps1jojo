bf6572d5cb0a6f1ad3ecedd5580b633679a36c9b
jojosarah273 <jojosarah273@users.noreply.github.com>
2026-09-08 10:25:01 +0000

    grind: menu-loop twins (2); C-written 1363

--- src/func_8011B4A4.c
+++ src/func_8011B4A4.c
@@ -1,12 +1,17 @@
 #include "common.h"
-__asm__(
-  ".globl func_8011B4A4\n"
-  ".type func_8011B4A4, @function\n"
-  "func_8011B4A4:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tsw $ra, 0x10($sp)\n\tjal func_8011B744\n\tnop\n\tjal func_800F654C\n\taddiu $a0, $zero, 0xFE\n\t.L8011B4BC:\n\tjal func_800F5A90\n\taddu $a0, $zero, $zero\n\tjal func_800F53D4\n\tnop\n\tbnez $v0, .L8011B4F4\n\tnop\n\tjal func_800F5480\n\tnop\n\tjal func_800F7C6C\n\tnop\n\tjal func_800F5EA0\n\tnop\n\tj .L8011B4BC\n\tnop\n\t.L8011B4F4:\n\tjal func_800F7270\n\taddiu $a0, $zero, 0x3D\n\tjal func_800F4960\n\taddiu $a0, $zero, 0x1280\n\tjal func_800F8768\n\taddiu $a0, $zero, 0x1280\n\tlw $ra, 0x10($sp)\n\tnop\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_8011B4A4, .-func_8011B4A4\n"
-);
+void func_8011B4A4(void)
+{
+    func_8011B744();
+    func_800F654C(0xFE);
+    do {
+        func_800F5A90(0);
+        if (func_800F53D4() != 0)
+            break;
+        func_800F5480();
+        func_800F7C6C();
+        func_800F5EA0();
+    } while (1);
+    func_800F7270(0x3D);
+    func_800F4960(0x1280);
+    func_800F8768(0x1280);
+}
--- src/func_8011B51C.c
+++ src/func_8011B51C.c
@@ -1,12 +1,17 @@
 #include "common.h"
-__asm__(
-  ".globl func_8011B51C\n"
-  ".type func_8011B51C, @function\n"
-  "func_8011B51C:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tsw $ra, 0x10($sp)\n\tjal func_8011B7B4\n\tnop\n\tjal func_800F654C\n\taddiu $a0, $zero, 0xFE\n\t.L8011B534:\n\tjal func_800F5A90\n\taddu $a0, $zero, $zero\n\tjal func_800F53D4\n\tnop\n\tbnez $v0, .L8011B56C\n\tnop\n\tjal func_800F5480\n\tnop\n\tjal func_800F7C6C\n\tnop\n\tjal func_800F5EA0\n\tnop\n\tj .L8011B534\n\tnop\n\t.L8011B56C:\n\tjal func_800F7270\n\taddiu $a0, $zero, 0x3D\n\tjal func_800F4A7C\n\taddiu $a0, $zero, 0x12E0\n\tjal func_800F885C\n\taddiu $a0, $zero, 0x12E0\n\tlw $ra, 0x10($sp)\n\tnop\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_8011B51C, .-func_8011B51C\n"
-);
+void func_8011B51C(void)
+{
+    func_8011B7B4();
+    func_800F654C(0xFE);
+    do {
+        func_800F5A90(0);
+        if (func_800F53D4() != 0)
+            break;
+        func_800F5480();
+        func_800F7C6C();
+        func_800F5EA0();
+    } while (1);
+    func_800F7270(0x3D);
+    func_800F4A7C(0x12E0);
+    func_800F885C(0x12E0);
+}
