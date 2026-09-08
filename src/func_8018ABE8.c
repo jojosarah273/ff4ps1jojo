e2b69c5e398d3dce5799be152507fcb43d347b82
jojosarah273 <jojosarah273@users.noreply.github.com>
2026-09-08 02:56:49 +0000

    grind: choice-dispatch + flag-bit setter (2); C-written 1290

--- src/func_8018ABE8.c
+++ src/func_8018ABE8.c
@@ -1,12 +1,8 @@
 #include "common.h"
-__asm__(
-  ".globl func_8018ABE8\n"
-  ".type func_8018ABE8, @function\n"
-  "func_8018ABE8:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\tlui $a1, %hi(D_8019B124)\n\tlw $a1, %lo(D_8019B124)($a1)\n\tlui $v1, (0xFFF8FFFF >> 16)\n\tlw $v0, 0x0($a1)\n\tori $v1, $v1, (0xFFF8FFFF & 0xFFFF)\n\tand $v0, $v0, $v1\n\tbeqz $a0, .L8018AC20\n\tsw $v0, 0x0($a1)\n\tlui $v0, %hi(D_8019B124)\n\tlw $v0, %lo(D_8019B124)($v0)\n\tnop\n\tlw $v1, 0x0($v0)\n\tj .L8018AC34\n\tlui $a0, (0x30000 >> 16)\n\t.L8018AC20:\n\tlui $v0, %hi(D_8019B124)\n\tlw $v0, %lo(D_8019B124)($v0)\n\tnop\n\tlw $v1, 0x0($v0)\n\tlui $a0, (0x50000 >> 16)\n\t.L8018AC34:\n\tor $v1, $v1, $a0\n\tjr $ra\n\tsw $v1, 0x0($v0)\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_8018ABE8, .-func_8018ABE8\n"
-);
+extern u32 *D_8019B124;
+u32 func_8018ABE8(u32 a0)
+{
+    u32 *p = D_8019B124;
+    *p &= 0xFFF8FFFF;
+    return a0 ? (*p | 0x30000) : (*p | 0x50000);
+}
--- src/func_8018B16C.c
+++ src/func_8018B16C.c
@@ -1,12 +1,13 @@
 #include "common.h"
-__asm__(
-  ".globl func_8018B16C\n"
-  ".type func_8018B16C, @function\n"
-  "func_8018B16C:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\tlui $v1, (0xFFFFFFF >> 16)\n\tori $v1, $v1, (0xFFFFFFF & 0xFFFF)\n\tlui $v0, (0x40000000 >> 16)\n\tand $v0, $a2, $v0\n\tand $a2, $a2, $v1\n\tsltu $v1,$zero,$v0\n\tslt $a2, $a2, $a0\n\tbeqz $a2, .L8018B1A8\n\taddu $v0, $v1, $zero\n\tbne $a1, $a3, .L8018B1B4\n\taddiu $v0, $zero, 0x2\n\tbeqz $v1, .L8018B1B4\n\taddiu $v0, $zero, 0x1\n\tj .L8018B1B4\n\taddiu $v0, $zero, 0x2\n\t.L8018B1A8:\n\tbeq $a1, $a3, .L8018B1B4\n\tnop\n\taddiu $v0, $zero, 0x1\n\t.L8018B1B4:\n\tjr $ra\n\tnop\n\tnop\n\tnop\n\tnop\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_8018B16C, .-func_8018B16C\n"
-);
+u32 func_8018B16C(u32 a0, u32 a1, u32 a2, u32 a3)
+{
+    u32 flag = (a2 & 0x40000000) != 0;
+    if ((a2 & 0x0FFFFFFFu) < a0) {
+        if (a1 != a3)
+            return 2;
+        return flag ? 2 : 1;
+    }
+    if (a1 != a3)
+        return 1;
+    return flag;
+}
