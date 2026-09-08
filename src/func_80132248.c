a671af1126c1f2b8faaa0ccd392337a20599d22c
jojosarah273 <jojosarah273@users.noreply.github.com>
2026-09-08 03:01:21 +0000

    grind: sign-compare, reorder-copy, menu loops (5); C-written 1325

--- src/func_80132248.c
+++ src/func_80132248.c
@@ -1,12 +1,14 @@
 #include "common.h"
-__asm__(
-  ".globl func_80132248\n"
-  ".type func_80132248, @function\n"
-  "func_80132248:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tsw $ra, 0x10($sp)\n\tjal func_800F6564\n\taddiu $a0, $zero, 0x1A04\n\tjal func_800F4248\n\taddiu $a0, $zero, 0x20\n\tjal func_800F4120\n\taddiu $a0, $zero, 0x2\n\tbeqz $v0, .L80132288\n\tnop\n\tjal func_80131294\n\tnop\n\tjal func_8012219C\n\tnop\n\tj .L8013229C\n\taddiu $v0, $zero, 0x2\n\t.L80132288:\n\tjal func_800F654C\n\taddiu $a0, $zero, 0x4\n\tjal func_80132310\n\tnop\n\taddiu $v0, $zero, 0x1\n\t.L8013229C:\n\tlw $ra, 0x10($sp)\n\tnop\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_80132248, .-func_80132248\n"
-);
+u32 func_80132248(void)
+{
+    func_800F6564(0x1A04);
+    func_800F4248(0x20);
+    if (func_800F4120(2) != 0) {
+        func_80131294();
+        func_8012219C();
+        return 2;
+    }
+    func_800F654C(4);
+    func_80132310();
+    return 1;
+}
--- src/func_801322AC.c
+++ src/func_801322AC.c
@@ -1,12 +1,14 @@
 #include "common.h"
-__asm__(
-  ".globl func_801322AC\n"
-  ".type func_801322AC, @function\n"
-  "func_801322AC:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tsw $ra, 0x10($sp)\n\tjal func_800F6564\n\taddiu $a0, $zero, 0x1A04\n\tjal func_800F4248\n\taddiu $a0, $zero, 0x40\n\tjal func_800F4120\n\taddiu $a0, $zero, 0x2\n\tbeqz $v0, .L801322EC\n\tnop\n\tjal func_80131294\n\tnop\n\tjal func_8012219C\n\tnop\n\tj .L80132300\n\taddiu $v0, $zero, 0x2\n\t.L801322EC:\n\tjal func_800F654C\n\taddiu $a0, $zero, 0x5\n\tjal func_80132310\n\tnop\n\taddiu $v0, $zero, 0x1\n\t.L80132300:\n\tlw $ra, 0x10($sp)\n\tnop\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_801322AC, .-func_801322AC\n"
-);
+u32 func_801322AC(void)
+{
+    func_800F6564(0x1A04);
+    func_800F4248(0x40);
+    if (func_800F4120(2) == 0) {
+        func_800F654C(5);
+        func_80132310();
+        return 1;
+    }
+    func_80131294();
+    func_8012219C();
+    return 2;
+}
