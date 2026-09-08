72ac3ac978cab29572360cb242033a0d323065c0
jojosarah273 <jojosarah273@users.noreply.github.com>
2026-09-08 10:25:30 +0000

    grind: table paint, countdown dispatch, slot removal, menu branch (4); C-written 1367

--- src/func_801316E0.c
+++ src/func_801316E0.c
@@ -1,12 +1,17 @@
 #include "common.h"
-__asm__(
-  ".globl func_801316E0\n"
-  ".type func_801316E0, @function\n"
-  "func_801316E0:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tsw $ra, 0x10($sp)\n\tjal func_800F6564\n\taddiu $a0, $zero, 0x1BBB\n\tjal func_800F6434\n\taddiu $a0, $zero, 0x2\n\tbnez $v0, .L80131720\n\tlui $a0, (0xF9807 >> 16)\n\tjal func_800F65C8\n\tori $a0, $a0, (0xF9807 & 0xFFFF)\n\tjal func_800F8188\n\taddiu $a0, $zero, 0x1B91\n\tjal func_801317F0\n\tnop\n\tj .L80131750\n\tnop\n\t.L80131720:\n\tjal func_800F654C\n\taddu $a0, $zero, $zero\n\t.L80131728:\n\tjal func_80131760\n\tnop\n\tjal func_800F61E8\n\tnop\n\tjal func_800F5574\n\taddiu $a0, $zero, 0x5\n\tjal func_800F53D4\n\tnop\n\tbeqz $v0, .L80131728\n\tnop\n\t.L80131750:\n\tlw $ra, 0x10($sp)\n\tnop\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_801316E0, .-func_801316E0\n"
-);
+void func_801316E0(void)
+{
+    func_800F6564(0x1BBB);
+    if (func_800F6434(2) == 0) {
+        func_800F65C8(0xF9807);
+        func_800F8188(0x1B91);
+        func_801317F0();
+        return;
+    }
+    func_800F654C(0);
+    do {
+        func_80131760();
+        func_800F61E8();
+        func_800F5574(5);
+    } while (func_800F53D4() == 0);
+}
--- src/func_80189590.c
+++ src/func_80189590.c
@@ -1,12 +1,17 @@
 #include "common.h"
-__asm__(
-  ".globl func_80189590\n"
-  ".type func_80189590, @function\n"
-  "func_80189590:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tsw $s0, 0x10($sp)\n\taddu $s0, $a0, $zero\n\taddiu $a0, $zero, 0x1\n\tsw $ra, 0x14($sp)\n\tjal func_801917F8\n\taddu $a1, $zero, $zero\n\taddu $v1, $v0, $zero\n\taddiu $v0, $zero, 0x2\n\tbeq $v1, $v0, .L801895CC\n\taddiu $v0, $zero, 0x5\n\tbeq $v1, $v0, .L801895F8\n\taddu $v0, $s0, $zero\n\tj .L801895FC\n\tnop\n\t.L801895CC:\n\taddu $a2, $zero, $zero\n\tlw $a1, %gp_rel(D_8019EF88)($gp)\n\tlw $v1, %gp_rel(D_8019ED28)($gp)\n\tlw $a0, %gp_rel(D_8019EF40)($gp)\n\tsll $v0, $a1, 4\n\taddu $v0, $v0, $v1\n\taddiu $v1, $zero, -0x1\n\tbeq $a1, $a0, .L801895F4\n\tsh $v1, 0x0($v0)\n\taddiu $a2, $a1, 0x1\n\t.L801895F4:\n\tsw $a2, %gp_rel(D_8019EF88)($gp)\n\t.L801895F8:\n\taddu $v0, $zero, $zero\n\t.L801895FC:\n\tlw $ra, 0x14($sp)\n\tlw $s0, 0x10($sp)\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_80189590, .-func_80189590\n"
-);
+extern u32 *D_8019EF88;
+extern u32 *D_8019ED28;
+extern u32 *D_8019EF40;
+u32 func_80189590(u32 a0)
+{
+    u32 r = func_801917F8(1, 0);
+    if (r == 2) {
+        u32 idx = *D_8019EF88;
+        *(s16 *)((u8 *)*D_8019ED28 + idx * 16) = -1;
+        *D_8019EF88 = (*D_8019EF88 == *D_8019EF40) ? *D_8019EF88 : (*D_8019EF88 + 1);
+        return 0;
+    }
+    if (r == 5)
+        return 0;
+    return a0;
+}
