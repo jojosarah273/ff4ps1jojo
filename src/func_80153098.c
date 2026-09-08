abe6c5f91eeb12f74e34a3c42bfea0f3cecc7fa9
jojosarah273 <jojosarah273@users.noreply.github.com>
2026-09-08 02:55:08 +0000

    grind: setter-callback, abs-mapper, bounds guards (5); C-written 1284

--- src/func_80153098.c
+++ src/func_80153098.c
@@ -1,12 +1,7 @@
 #include "common.h"
-__asm__(
-  ".globl func_80153098\n"
-  ".type func_80153098, @function\n"
-  "func_80153098:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tlui $v0, %hi(D_8019ED54)\n\tlw $v0, %lo(D_8019ED54)($v0)\n\tlui $a0, 0x13\n\tsw $ra, 0x10($sp)\n\tlhu $a1, 0x0($v0)\n\tjal func_800F3B9C\n\tori $a0, $a0, 0xFEFE\n\tlui $v1, %hi(D_8019ED54)\n\tlw $v1, %lo(D_8019ED54)($v1)\n\tnop\n\tlhu $a0, 0x0($v1)\n\tjal func_800F78C4\n\taddu $a0, $v0, $a0\n\tlw $ra, 0x10($sp)\n\tnop\n\tjr $ra\n\taddiu $sp, $sp, 0x18\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_80153098, .-func_80153098\n"
-);
+extern u16 *D_8019ED54;
+void func_80153098(void)
+{
+    u16 v = *D_8019ED54;
+    func_800F78C4(func_800F3B9C(0x13FEFE, v) + v);
+}
--- src/func_80197EE8.c
+++ src/func_80197EE8.c
@@ -1,12 +1,7 @@
 #include "common.h"
-__asm__(
-  ".globl func_80197EE8\n"
-  ".type func_80197EE8, @function\n"
-  "func_80197EE8:\n"
-  "\t.set\tnoreorder\n"
-  "\t.set noreorder\n"
-  "\taddiu $sp, $sp, -0x18\n\tbltz $a0, .L80197F04\n\tsw $ra, 0x10($sp)\n\tjal func_80197F24\n\tandi $a0, $a0, 0xFFF\n\tj .L80197F14\n\tnop\n\t.L80197F04:\n\tnegu $a0, $a0\n\tjal func_80197F24\n\tandi $a0, $a0, 0xFFF\n\tnegu $v0, $v0\n\t.L80197F14:\n\tlw $ra, 0x10($sp)\n\taddiu $sp, $sp, 0x18\n\tjr $ra\n\tnop\n"
-  "\t.set reorder\n"
-  "\t.set\treorder\n"
-  ".size func_80197EE8, .-func_80197EE8\n"
-);
+u32 func_80197EE8(s32 a0)
+{
+    if (a0 < 0)
+        return (u32)(-func_80197F24((u32)-a0 & 0xFFF));
+    return (u32)func_80197F24((u32)a0 & 0xFFF);
+}
